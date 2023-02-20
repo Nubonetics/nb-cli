6bdb6709aa0def0f4a78050e5bdc40659629fc84
Behzad Samadi <behzad.samadi@gmail.com>
2023-02-19 21:45:33 -0500

    Functions

--- nb_cli/system/app.py
+++ nb_cli/system/app.py
@@ -2,12 +2,14 @@
 import typer
 
 from nb_cli.cli import console
+
+# Functions
 from nb_cli.system.fcn.info import system_info
 
 system_app = typer.Typer()
 
-
+# Commands
 @system_app.command()
 def info() -> None:
-    """Display system properties."""
+    """Display system information."""
     console.print(system_info())
